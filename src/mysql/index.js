6a185bc56e8a2e66ac6293c0cd58c0a9be525906
tjjone98 <tzero1898@gmail.com>
2020-06-25 20:03:18 +0700

    fix issue

--- src/mysql/index.js
+++ src/mysql/index.js
@@ -8,26 +8,26 @@ const connection = mysql.createConnection({
 
 connection.connect((err) => {
 	if (err) throw err;
-});
 
-connection.query(
-	`CREATE TABLE \`dkmh\` (
+	connection.query(
+		`CREATE TABLE \`dkmh\` (
   \`mssv\` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
   \`maDK\` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
   \`maMH\` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (\`maDK\`,\`mssv\`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;`,
-	(error) => {}
-);
+		(error) => {}
+	);
 
-connection.query(
-	`CREATE TABLE \`lichdk\` (
+	connection.query(
+		`CREATE TABLE \`lichdk\` (
   \`dkLoc\` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
   \`value\` longtext COLLATE utf8_unicode_ci,
   PRIMARY KEY (\`dkLoc\`),
   UNIQUE KEY \`dkLoc_UNIQUE\` (\`dkLoc\`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;`,
-	(error) => {}
-);
+		(error) => {}
+	);
+});
 
 module.exports = connection;
