7d50ed8eb58287e3782db4fddf4cc868490e649f
tjjone98 <tzero1898@gmail.com>
2020-06-25 20:24:46 +0700

    fix connect database

--- src/mysql/index.js
+++ src/mysql/index.js
@@ -1,33 +1,35 @@
 const mysql = require('mysql');
-const connection = mysql.createConnection({
-	host: 'us-cdbr-east-05.cleardb.net',
+const connection = mysql.createPool({
+	host: process.env.DATABASE_HOST || 'us-cdbr-east-05.cleardb.net',
 	user: 'bd353e425b0945',
 	password: '7e7bf3c4',
 	database: 'heroku_64c5025f16fdbd2',
 });
 
 connection.connect((err) => {
-	if (err) throw err;
+	if (err) {
+		console.log(err);
+	}
+});
 
-	connection.query(
-		`CREATE TABLE \`dkmh\` (
+connection.query(
+	`CREATE TABLE \`dkmh\` (
   \`mssv\` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
   \`maDK\` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
   \`maMH\` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (\`maDK\`,\`mssv\`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;`,
-		(error) => {}
-	);
+	(error) => {}
+);
 
-	connection.query(
-		`CREATE TABLE \`lichdk\` (
+connection.query(
+	`CREATE TABLE \`lichdk\` (
   \`dkLoc\` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
   \`value\` longtext COLLATE utf8_unicode_ci,
   PRIMARY KEY (\`dkLoc\`),
   UNIQUE KEY \`dkLoc_UNIQUE\` (\`dkLoc\`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;`,
-		(error) => {}
-	);
-});
+	(error) => {}
+);
 
 module.exports = connection;
